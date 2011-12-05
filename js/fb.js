5791392c6e0af04d0b8f08ac5d78bc2dbfee809b
Keith Carpenter <keith.carpenter2@gmail.com>
2011-12-05 15:49:28 -0600

    home update, prototypes added

--- js/fb.js
+++ js/fb.js
@@ -1,8 +1,11 @@
 //globals
-var _teams= [], _people=[]; 
+var _teams= [], _people=[], _conferences=[];
 
 //init
 function init() {
+	//load conferences
+	loadConferences(); 
+	
 	//load teams
 	loadTeams(); 
 
@@ -20,6 +23,29 @@ function loadPeople() {
 }
 
 
+
+
+// ------- conferences
+function loadConferences() {
+	var conf1 = new Conference("SEC", "SEC"); 
+	_conferences.push(conf1); 
+	 conf1 = new Conference("PAC12", "PAC12"); 
+	_conferences.push(conf1); 
+	
+
+}
+
+// ------- teams
+function loadTeams() {
+	var team1 = new Team("LSU", "LSU", "Baton Rouge", "LA", "SEC"); 
+	_teams.push(team1); 
+	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
+	_teams.push(team1); 
+	
+
+}
+// objects - go in a separate class
+
 //player object (factory) 
 var Player = (function() {
    var Player= function (firstName, lastName, birthYear, team){
@@ -35,55 +61,7 @@ var Player = (function() {
 })();
 //todo: should extend a person object
 
-
-
-// ------- teams
-function loadTeams() {
-	var team1 = new Team("LSU", "LSU", "Baton Rouge", "LA", "SEC"); 
-	_teams.push(team1); 
-	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-	
-	team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-
-	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-
-	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-
-	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-
-	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-
-	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-
-	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-
-	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-
-	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-
-	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-
-	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-
-	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-	 team1 = new Team("Colorado", "CU", "Boulder", "CO", "PAC12"); 
-	_teams.push(team1); 
-
-}
-
+//give teams an id, assign players to teams after id's are set. players will have names of teams, so stlill need a get by name function. after that, use an array with an id key: columnObjects['321'] = new aColumn('321', 'Todds Column'); - then do columnObjects[id]; 
 
 var Team = (function() {
    var Team= function (name, abbreviation, city, state, conference){
@@ -98,4 +76,65 @@ var Team = (function() {
    };
 })();
 
+var Conference = (function() {
+   var Conference= function (name, abbreviation){  //stadium, divisions, championship game
+       this.name = name;
+       this.abbreviation = abbreviation ;
+   };
+   return function(name, abbreviation) {
+       return new Conference(name, abbreviation);
+   };
+})();
+
+
+//helper utils - move somewhere else
+Array.prototype.contains = function (itemId){
+	for (index in this) {  //todo check speed
+		if (this[index].name === itemId)
+		{
+			return true; 
+		}
+	}
+	return false; 
+	};
+ //todo: can i write a generic contains and override with custom lookup functions? 
+
+ Array.prototype.find = function (itemId){
+	for (index in this) {  //todo check speed
+		if (this[index].name === itemId)
+		{
+			return this[index]; 
+		}
+	}
+	return null; 
+};
+
+if (!Array.prototype.filter)
+{
+	Array.prototype.filter = function (fun) 
+	{
+		 "use strict";  
+  
+		if (this === void 0 || this === null)  
+		  throw new TypeError();  
+	  
+		var t = Object(this);  
+		var len = t.length >>> 0;  
+		if (typeof fun !== "function")  
+		  throw new TypeError();  
+	  
+		var res = [];  
+		var thisp = arguments[1];  
+		for (var i = 0; i < len; i++)  
+		{  
+		  if (i in t)  
+		  {  
+			var val = t[i]; // in case fun mutates this  
+			if (fun.call(thisp, val, i, t))  
+			  res.push(val);  
+		  }  
+		}  
+    return res;  
+	}
+}
 
--- js/ui.js
+++ js/ui.js
@@ -1,36 +1,128 @@
-//ui events here
+//ui pub sub events here
 $.subscribe(fbEvents.INIT_COMPLETE, function ()
 		{
 			clearScreen(); 
-			renderTeams(_teams);  //teams should be in the data tier
-			renderPlayers(_people); 
+			//show intro screen or something
+			listAllTeams(_teams);  //teams should be in the data tier
+			listAllPlayers(_people); 
 			$.publish(fbEvents.DATA_LOAD_COMPLETE); 
-			_bindEvents(); 
 		});
+		
+		
+$.subscribe(fbEvents.DATA_LOAD_COMPLETE, function ()
+		{
+			bindEvents(); 
+		});
+		
 
+		
+//ui functions here 
 function clearScreen(){
 	$("#mainPane").empty();  
 }
 
-
-function renderTeams(data) { 
-	if (data)
+function listTeamsByConference(conference) 
+{
+	if (conference) 
 	{
-		
-		//if not data, error
-		//$("#mainPane").empty();  
+		//filter teams by conference
+		function isInConference(element, index, array) { 
+			return (element.conference ===conference); 
+		}
+		var data = _teams.filter(isInConference); 
 		$("#tmpl-teams-list").tmpl({teams: data}).appendTo("#mainPane"); 
-
 	}
 }
 
-function renderPlayers(data) { 
+function listAllTeams(data) { 
+	if (data)//if not data, error
+	{
+		$("#tmpl-teams-list").tmpl({teams: data}).appendTo("#mainPane"); 
+	}
+}
+//could have a displayItem function, and pass in template, array...
+function displayTeam(teamId){
+	clearScreen(); 
+	//if (_teams.contains(teamId)) 
+	//{alert("YES");}
+	var team = _teams.find(teamId); 
+	if (team) 
+	{
+		$("#tmpl-team-detail").tmpl(team).appendTo("#mainPane"); 
+	} //TODO: else, error handle
+
+}
+
+function displayConference(conferenceId){
+	clearScreen(); 
+	var conference = _conferences.find(conferenceId); 
+	if (conference) 
+	{
+		$("#tmpl-conference-detail").tmpl(conference).appendTo("#mainPane"); 
+	} //TODO: else, error handle
+
+}
+
+function listPlayersByTeam(team){
+	if (team) 
+	{
+		//filter teams by conference
+		function isOnTeam(element, index, array) { 
+			return (element.team ===team); 
+		}
+		var data = _people.filter(isOnTeam); 
+		$("#tmpl-players-list").tmpl({team: team, players: data}).appendTo("#mainPane"); 
+	}
+}
+
+function listAllPlayers(data) { 
 	if (data)
 	{
 		$("#tmpl-players-list").tmpl({players: data}).appendTo("#mainPane"); 
 	}
 }
+
+//ui event binding here 
+/*! bindEvents */ 		
+function bindEvents(){
+	//team links
+	$("body").delegate("[data-uitype='link-to-item']", "click", function(evt)  { 
+		evt.preventDefault(); 
+		var itemId = $(this).data("item-id"); 
+		var itemType = $(this).data("item-type"); 
 		
-function _bindEvents(){
+		switch (itemType)
+		{
+			case "team" : 
+				displayTeam(itemId); 
+				break; 
+			case "conference" :
+				displayConference(itemId); 
+				break; 
+		}
+		//modal, keep history, give 2 links (modal, new window)? 
+	}); 
+	
+	$("body").delegate("[data-uitype='link-to-list']", "click", function(evt)  { 
+		evt.preventDefault(); 
+		var itemId = $(this).data("item-id"); 
+		var itemType = $(this).data("item-type"); 
+		
+		switch (itemType)
+		{
+			case "team" : 
+				listTeamsByConference(itemId); 
+				break; 
+			case "conference" :
+				listConferences(itemId); 
+				break; 
+			case "player" : 
+				listPlayersByTeam(itemId); 
+				break; 
+		}
+		//modal, keep history, give 2 links (modal, new window)? 
+	}); 
+	
+	
 	
 }
