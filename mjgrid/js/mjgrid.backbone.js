b808b115955f2e468a0209db04b517df54391b53
Keith Carpenter <keith.carpenter2@gmail.com>
2012-02-07 16:55:12 -0600

    tinkering with bbone

--- mjgrid/js/mjgrid.backbone.js
+++ mjgrid/js/mjgrid.backbone.js
@@ -15,22 +15,55 @@ var CompareItems = Backbone.Collection.extend({
 	model : CompareItem
 });
 
+var CompareView = Backbone.View.extend({
+	initialize: function() { 
+		this.render(); 
+	},
+	render: function() { 
+		var tmplHtml = $("#backBoneTemplate").html(); 
+		var template = _.template(tmplHtml); 
+		this.el.html (template); 
+	}
+}); 
+
+
+
+//*** Loading data ***//
 var compareItem1 = new CompareItem({
 	name: "Tablet1", 
 	imageUrl:"http://g-ecx.images-amazon.com/images/G/01/kindle/otter/dp/KO-slate-02-sm._V164817994_.jpg",
-	comparableAttributes: [{"name":"screensize", "value":"12", "uitype":"checkbox"},{"name":"speed", "value":"fast", "uitype":"slider"}]
+	comparableAttributes: [{screensize:"12"},{speed:"fast"}]
 }); 
 var compareItem2 = new CompareItem({
 	name: "Tablet2", 
 	imageUrl:"http://ecx.images-amazon.com/images/I/513JK4cckxL._SL500_AA300_.jpg",
-	comparableAttributes: [{"name":"screensize", "value":"12", "uitype":"checkbox"},{"name":"speed", "value":"fast", "uitype":"slider"}]	
+	comparableAttributes: [{screensize:"7"},{speed:"fast"}]
+	//comparableAttributes: {"screensize":"12","speed":"fast"}
 }); 
 var compareItem3 = new CompareItem({
 	name: "Tablet3", 
 	imageUrl:"http://ecx.images-amazon.com/images/I/41yBWI7-OwL._SL500_AA300_.jpg",
-	comparableAttributes: [{"name":"screensize", "value":"12", "uitype":"checkbox"},{"name":"speed", "value":"fast", "uitype":"slider"}]
+	comparableAttributes: [{screensize:"12"},{speed:"fast"}]
+	
 }); 
 
-var compareItems = new CompareItems([compareItem1, compareItem2, compareItem3]); 
+//JSON model data
+var compareJSON= [
+{
+	name:"Tablet4", imageUrl:"", comparableAttributes:[{screensize:"15"},{speed:"slow"}]
+},
+{
+	name:"Tablet5", imageUrl:"", comparableAttributes:[{screensize:"14"},{speed:"medium"}]
+}
+];
+//from json
+var compareItems = new CompareItems(
+_.map(compareJSON, function(item) { return item }));
+
+//add object literals
+compareItems.add([compareItem1, compareItem2, compareItem3]); 
 
 console.log (compareItems.models); 
+
+var compareView = new CompareView({ el: $("#backboneGrid")}); 
+
